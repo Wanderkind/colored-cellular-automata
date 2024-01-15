dc40c2de6ffde0c479482249ef107f8432a7b37f
Jeongseop Lee <wanderkind@semyung.ac.kr>
2024-01-15 11:19:16 +0900

    Update basic_template.py
    adding comments for experimental divergences

--- basic_template.py
+++ basic_template.py
@@ -1,33 +1,37 @@
 import pygame
 import numpy as np
 
-grid_width, grid_height = 200, 100
+grid_width, grid_height = 220, 100
 cell_size = 8
 
 grid = np.zeros((grid_width, grid_height), dtype = int)
 
 center = grid_width//2
 start_cell = (center, 1)
-grid[start_cell] = 7
+#grid[start_cell] = 63
+grid[start_cell] = 15
+#grid[start_cell] = 7
 
 pygame.init()
 
 screen_width = grid_width * cell_size
 screen_height = grid_height * cell_size
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Cellular Automata")
 
 white = (255, 255, 255)
 black = (0, 0, 0)
 
-f = lambda x: (x//4, (x % 4)//2, x % 2)
+f = lambda x: (x//16, (x % 16)//4, x % 4)
+#f = lambda x: (x//4, (x % 4)//2, x % 2)
 
 def rule(a, b, c):
     a1, a2, a3 = f(a)
     b1, b2, b3 = f(b)
     c1, c2, c3 = f(c)
-    res = (a1^a2^a3)*4 + (b1^b2^b3)*2 + (c1^c2^c3)*1
-    return res
+    res = (a1^a2^a1)*16 + (b1^b2^b3)*4 + (c1^c2^c3)*1
+    #res = (a1^a2^a1)*16 + (b1^b2^b3)*3 + (c1^c2^c3)*1
+    #res = (a1^a2^a1)*4 + (b1^b2^b3)*2 + (c1^c2^c3)*1
+    return res % 64
     #return res % 8
 
 def update_grid(current_grid):
@@ -38,6 +42,7 @@ def update_grid(current_grid):
                 new_grid[j, i] = current_grid[j, i]
             else:
                 new_grid[j, i] = rule(current_grid[j - 1, i - 1], current_grid[j, i - 1], current_grid[j + 1, i - 1])
+                #new_grid[j, i] = rule(current_grid[j - 1, i - 1], current_grid[j, i - 1], current_grid[j + 2, i - 1])
     return new_grid
 
 running = True
@@ -53,7 +58,8 @@ while running:
         for j in range(grid_height):
             g = grid[i, j]
             a, b, c = f(g)
-            color = (255*a, 255*b, 255*c)
+            color = (85*a, 85*b, 85*c)
+            #color = (255*a, 255*b, 255*c)
             pygame.draw.rect(screen, color, (i * cell_size, j * cell_size, cell_size, cell_size))
 
     pygame.display.flip()
